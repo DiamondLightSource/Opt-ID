1052840d37142ad7b3d97c4ab1b63e00bd69a96d
eddrial <eddrial@hotmail.com>
2015-05-27 16:51:14 +0100

    APPLE Blocks now calculated with chamfers removed

--- IDSort/src/v2/lookup_generator.py
+++ IDSort/src/v2/lookup_generator.py
@@ -32,19 +32,50 @@ if __name__ == "__main__":
     
     outfile = h5py.File(args[1], 'w')
     
-    for b in range(len(data['beams'])):
-        count = 0
-        print("Processing beam %02i" % (b))
-        datashape = (testpoints.shape[1], testpoints.shape[2], testpoints.shape[3], 3, 3, len(data['beams'][b]['mags']))
-        chunkshape = (testpoints.shape[1], testpoints.shape[2], testpoints.shape[3], 3, 3, 1)
-        print ("datashape is : " + str(datashape))
-        ds = outfile.create_dataset(data['beams'][b]['name'], shape=datashape, dtype=np.float64, chunks=chunkshape)
+    if data['type'] == 'PPM_AntiSymmetric':
+    
+        for b in range(len(data['beams'])):
+            count = 0
+            print("Processing beam %02i" % (b))
+            datashape = (testpoints.shape[1], testpoints.shape[2], testpoints.shape[3], 3, 3, len(data['beams'][b]['mags']))
+            chunkshape = (testpoints.shape[1], testpoints.shape[2], testpoints.shape[3], 3, 3, 1)
+            print ("datashape is : " + str(datashape))
+            ds = outfile.create_dataset(data['beams'][b]['name'], shape=datashape, dtype=np.float64, chunks=chunkshape)
+    
+            for mag in data['beams'][b]['mags']:
+                print("processing beam %02i magnet %04i" % (b, count))
+                dataset = mt.wrapCalcB(testpoints, np.array(mag['dimensions']), np.array(mag['position']))
+                ds[:, :, :, :, :, count] = dataset.dot(np.array(mag['direction_matrix']))
+                count += 1
+                
+        outfile.close()
+        
+    if data['type'] == 'APPLE_Symmetric':
+    
+        for b in range(len(data['beams'])):
+            count = 0
+            print("Processing beam %02i" % (b))
+            datashape = (testpoints.shape[1], testpoints.shape[2], testpoints.shape[3], 3, 3, len(data['beams'][b]['mags']))
+            chunkshape = (testpoints.shape[1], testpoints.shape[2], testpoints.shape[3], 3, 3, 1)
+            print ("datashape is : " + str(datashape))
+            ds = outfile.create_dataset(data['beams'][b]['name'], shape=datashape, dtype=np.float64, chunks=chunkshape)
+    
+            for mag in data['beams'][b]['mags']:
+                print("processing beam %02i magnet %04i" % (b, count))
+                datasetblock = mt.wrapCalcB(testpoints, np.array(mag['dimensions']), np.array(mag['position']))
+                if b%2==0:
+                    c2pos = np.array(mag['position'])+np.array([mag['dimensions'][0]-data['clampcut'],mag['dimensions'][1]-data['clampcut'],0.0])
+                    datasetc1 = mt.wrapCalcB(testpoints, np.array([data['clampcut'],data['clampcut'],mag['dimensions'][2]]), np.array(mag['position']))
+                    datasetc2 = mt.wrapCalcB(testpoints, np.array([data['clampcut'],data['clampcut'],mag['dimensions'][2]]), c2pos)
+                if b%2==1:
+                    c1pos = np.array(mag['position'])+np.array([mag['dimensions'][0]-data['clampcut'], 0.0, 0.0])
+                    c2pos = np.array(mag['position'])+np.array([0.0 ,mag['dimensions'][1]-data['clampcut'], 0.0])
+                    datasetc1 = mt.wrapCalcB(testpoints, np.array([data['clampcut'],data['clampcut'],mag['dimensions'][2]]), np.array(mag['position']))
+                    datasetc2 = mt.wrapCalcB(testpoints, np.array([data['clampcut'],data['clampcut'],mag['dimensions'][2]]), np.array(mag['position']))
+                dataset=datasetblock-datasetc1-datasetc2
+                ds[:, :, :, :, :, count] = dataset.dot(np.array(mag['direction_matrix']))
+                count += 1
+                
+        outfile.close()
 
-        for mag in data['beams'][b]['mags']:
-            print("processing beam %02i magnet %04i" % (b, count))
-            dataset = mt.wrapCalcB(testpoints, np.array(mag['dimensions']), np.array(mag['position']))
-            ds[:, :, :, :, :, count] = dataset.dot(np.array(mag['direction_matrix']))
-            count += 1
-            
-    outfile.close()
 
