693fbaf171d4643964cd0979e3ae97ac6fab9624
dtailor17 <dharmesh.tailor@live.co.uk>
2015-07-28 16:43:44 +0100

    Corrected rm command in generate_lookup_h5 to absolute path

--- IDSort/data_I21/scripts/generate_lookup_h5.sh
+++ IDSort/data_I21/scripts/generate_lookup_h5.sh
@@ -9,5 +9,5 @@ python $DIR/IDSort/src/v2/lookup_generator.py \
     $DIR/IDSort/data_I21/I21.json \
     /scratch/I21lookup_a.h5
 
-rm I21lookup_a.h5 &> /dev/null
+rm $DIR/IDSort/data_I21/I21lookup_a.h5 &> /dev/null
 ln -s /scratch/I21lookup_a.h5 $DIR/IDSort/data_I21/I21lookup_a.h5
