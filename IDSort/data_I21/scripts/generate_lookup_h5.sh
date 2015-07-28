43042f75bc51dc3d5541ee5bf6917bf4a992a568
dtailor17 <dharmesh.tailor@live.co.uk>
2015-07-28 16:40:30 +0100

    Added symbolic link to lookup file

--- IDSort/data_I21/scripts/generate_lookup_h5.sh
+++ IDSort/data_I21/scripts/generate_lookup_h5.sh
@@ -7,5 +7,7 @@ python $DIR/IDSort/src/v2/lookup_generator.py \
     -p 91 \
     -r \
     $DIR/IDSort/data_I21/I21.json \
-    /scratch/I21lookup_a.h
+    /scratch/I21lookup_a.h5
 
+rm I21lookup_a.h5 &> /dev/null
+ln -s /scratch/I21lookup_a.h5 $DIR/IDSort/data_I21/I21lookup_a.h5
