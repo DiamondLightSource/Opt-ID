bbfb6fc175a441e409eaf418bb242570b17bf3b4
dtailor17 <dharmesh.tailor@live.co.uk>
2015-08-12 16:31:05 +0100

    Created view for JSON ID description generator

---
+++ uk.ac.diamond.optid/src/uk/ac/diamond/optid/views/IdDescForm.java
@@ -0,0 +1,24 @@
+package uk.ac.diamond.optid.views;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.part.ViewPart;
+
+public class IdDescForm extends ViewPart {
+
+	public IdDescForm() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void createPartControl(Composite parent) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setFocus() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
