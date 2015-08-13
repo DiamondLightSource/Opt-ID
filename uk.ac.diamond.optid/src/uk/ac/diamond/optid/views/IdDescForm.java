4dc391c20ef47c620906b55b2990ec859e84c918
dtailor17 <dharmesh.tailor@live.co.uk>
2015-08-13 10:46:23 +0100

    UI fields for ID parameters created

--- uk.ac.diamond.optid/src/uk/ac/diamond/optid/views/IdDescForm.java
+++ uk.ac.diamond.optid/src/uk/ac/diamond/optid/views/IdDescForm.java
@@ -1,6 +1,13 @@
 package uk.ac.diamond.optid.views;
 
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Group;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.part.ViewPart;
 
 public class IdDescForm extends ViewPart {
@@ -11,8 +18,42 @@ public class IdDescForm extends ViewPart {
 
 	@Override
 	public void createPartControl(Composite parent) {
-		// TODO Auto-generated method stub
+		// Top-level composite
+		Composite mainComposite = new Composite(parent, SWT.NONE);
+		mainComposite.setLayout(new GridLayout(1, false));
+		mainComposite.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
+		
+		// Group - ID Parameters
+		Group grpIdParams = new Group(mainComposite, SWT.NONE);
+		grpIdParams.setText("ID Parameters");
+		grpIdParams.setLayout(new GridLayout(2, false));
+		grpIdParams.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, false));
+		
+		// Text (String) Field - Name
+		(new Label(grpIdParams, SWT.NONE)).setText("Name");
+		Text txtName = new Text(grpIdParams, SWT.SINGLE | SWT.BORDER);
+		txtName.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+		
+		// Combo (String) Field - Type
+		(new Label(grpIdParams, SWT.NONE)).setText("Type");
+		Combo cboType = new Combo(grpIdParams, SWT.READ_ONLY);
+		cboType.setItems(new String[] {"PPM AntiSymmetric", "APPLE Symmetric"});
+		cboType.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+		
+		// Text (int) Field - Periods
+		(new Label(grpIdParams, SWT.NONE)).setText("Periods");
+		Text txtPeriods = new Text(grpIdParams, SWT.SINGLE | SWT.BORDER);
+		txtPeriods.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
 
+		// Text (float) Field - Gap
+		(new Label(grpIdParams, SWT.NONE)).setText("Gap");
+		Text txtGap = new Text(grpIdParams, SWT.SINGLE | SWT.BORDER);
+		txtGap.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+		
+		// Text (float) Field - Interstice
+		(new Label(grpIdParams, SWT.NONE)).setText("Interstice");
+		Text txtInterstice = new Text(grpIdParams, SWT.SINGLE | SWT.BORDER);
+		txtInterstice.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));	
 	}
 
 	@Override
