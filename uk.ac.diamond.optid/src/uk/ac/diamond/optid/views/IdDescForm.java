dfcff960c98cbed0afce4802659a4ce3a68e8a2e
dtailor17 <dharmesh.tailor@live.co.uk>
2015-08-28 14:16:59 +0100

    Tidied & commented UI code in IdDescForm view

--- uk.ac.diamond.optid/src/uk/ac/diamond/optid/views/IdDescForm.java
+++ uk.ac.diamond.optid/src/uk/ac/diamond/optid/views/IdDescForm.java
@@ -7,18 +7,12 @@ import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.Label;
-import org.eclipse.swt.widgets.Table;
-import org.eclipse.swt.widgets.TableColumn;
-import org.eclipse.swt.widgets.TableItem;
 import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.part.ViewPart;
 
 public class IdDescForm extends ViewPart {
 	
 	static final String ID = "uk.ac.diamond.optid.idDescForm";
-
-	public IdDescForm() {
-	}
 
 	@Override
 	public void createPartControl(Composite parent) {
@@ -34,6 +28,10 @@ public class IdDescForm extends ViewPart {
 		setupAppleSymOnlyParams(mainComposite);
 	}
 
+	/**
+	 * Setup group of text fields to input general ID parameters
+	 * @param parent
+	 */
 	private void setupIdParams(Composite parent) {
 		// Group - ID Parameters
 		Group grpIdParams = new Group(parent, SWT.NONE);
@@ -44,13 +42,11 @@ public class IdDescForm extends ViewPart {
 		// Text (String) Field - Name
 		(new Label(grpIdParams, SWT.NONE)).setText("Name");
 		Text txtName = new Text(grpIdParams, SWT.SINGLE | SWT.BORDER);
-		txtName.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
 		
 		// Combo (String) Field - Type
 		(new Label(grpIdParams, SWT.NONE)).setText("Type");
 		Combo cboType = new Combo(grpIdParams, SWT.READ_ONLY);
 		cboType.setItems(new String[] {"PPM AntiSymmetric", "APPLE Symmetric"});
-		cboType.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
 		
 		// Text (int) Field - Periods
 		(new Label(grpIdParams, SWT.NONE)).setText("Periods");
@@ -60,18 +56,27 @@ public class IdDescForm extends ViewPart {
 		// Text (float) Field - Gap
 		(new Label(grpIdParams, SWT.NONE)).setText("Gap");
 		Text txtGap = new Text(grpIdParams, SWT.SINGLE | SWT.BORDER);
-		txtGap.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
 		
 		// Text (float) Field - Interstice
 		(new Label(grpIdParams, SWT.NONE)).setText("Interstice");
 		Text txtInterstice = new Text(grpIdParams, SWT.SINGLE | SWT.BORDER);
-		txtInterstice.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));			
+		
+		// Make text box stretch to fill width of view
+		txtName.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+		cboType.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+		txtGap.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+		txtInterstice.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));	
 	}
 	
+	/**
+	 * Setup group to input magnet dimensions
+	 * @param parent
+	 */
 	private void setupMagnetDims(Composite parent) {
 		// Group - Magnet Dimensions
 		Group grpMagDims = new Group(parent, SWT.NONE);
 		grpMagDims.setText("Magnet Dimensions");
+		// Each row has 4 cells: label followed by 3 text boxes
 		grpMagDims.setLayout(new GridLayout(4, false));
 		grpMagDims.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, false));
 		
@@ -90,29 +95,50 @@ public class IdDescForm extends ViewPart {
 		lblColTitle.setText("S");
 		lblColTitle.setLayoutData(new GridData(SWT.CENTER, SWT.FILL, true, false));
 		
-		// Full magnet block
+		/* Full magnet block */
 		(new Label(grpMagDims, SWT.NONE)).setText("Full");
+		// Text (float) Field - Full X
 		Text txtFullX = new Text(grpMagDims, SWT.SINGLE | SWT.BORDER);
+		
+		// Text (float) Field - Full Z
 		Text txtFullZ = new Text(grpMagDims, SWT.SINGLE | SWT.BORDER);
+
+		// Text (float) Field - Full S
 		Text txtFullS = new Text(grpMagDims, SWT.SINGLE | SWT.BORDER);
+		
+		// Make text box stretch to fill width of view
 		txtFullX.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
 		txtFullZ.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
 		txtFullS.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
 		
-		// VE magnet block
+		/* VE magnet block */
 		(new Label(grpMagDims, SWT.NONE)).setText("VE");
+		// Text (float) Field - VE X
 		Text txtVeX = new Text(grpMagDims, SWT.SINGLE | SWT.BORDER);
+		
+		// Text (float) Field - VE Z
 		Text txtVeZ = new Text(grpMagDims, SWT.SINGLE | SWT.BORDER);
+
+		// Text (float) Field - VE S
 		Text txtVeS = new Text(grpMagDims, SWT.SINGLE | SWT.BORDER);
+		
+		// Make text box stretch to fill width of view
 		txtVeX.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
 		txtVeZ.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
 		txtVeS.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
 		
-		// HE magnet block
+		/* HE magnet block */
 		(new Label(grpMagDims, SWT.NONE)).setText("HE");
+		// Text (float) Field - HE X
 		Text txtHeX = new Text(grpMagDims, SWT.SINGLE | SWT.BORDER);
+		
+		// Text (float) Field - HE Z
 		Text txtHeZ = new Text(grpMagDims, SWT.SINGLE | SWT.BORDER);
+
+		// Text (float) Field - HE S
 		Text txtHeS = new Text(grpMagDims, SWT.SINGLE | SWT.BORDER);
+		
+		// Make text box stretch to fill width of view
 		txtHeX.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
 		txtHeZ.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
 		txtHeS.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
@@ -127,6 +153,7 @@ public class IdDescForm extends ViewPart {
 		
 		// Composite - X & Z Container
 		Composite comp1 = new Composite(grpCalParams, SWT.NONE);
+		// Each row has 4 cells: label followed by 3 text boxes
 		comp1.setLayout(new GridLayout(4, false));
 		comp1.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, false));
 		
@@ -145,30 +172,45 @@ public class IdDescForm extends ViewPart {
 		lblColTitle.setText("Step");
 		lblColTitle.setLayoutData(new GridData(SWT.CENTER, SWT.FILL, true, false));
 		
-		// X Start-Stop-Step
+		/* X Start-Stop-Step */
 		(new Label(comp1, SWT.NONE)).setText("X");
+		// Text (float) Field - X Start
 		Text txtXStart = new Text(comp1, SWT.SINGLE | SWT.BORDER);
+		
+		// Text (float) Field - X Stop
 		Text txtXStop = new Text(comp1, SWT.SINGLE | SWT.BORDER);
+
+		// Text (float) Field - X Step
 		Text txtXStep = new Text(comp1, SWT.SINGLE | SWT.BORDER);
+		
+		// Make text box stretch to fill width of view
 		txtXStart.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
 		txtXStop.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
 		txtXStep.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
 		
-		// Z Start-Stop-Step
+		/* Z Start-Stop-Step */
 		(new Label(comp1, SWT.NONE)).setText("Z");
+		// Text (float) Field - Z Start
 		Text txtZStart = new Text(comp1, SWT.SINGLE | SWT.BORDER);
+
+		// Text (float) Field - Z Stop
 		Text txtZStop = new Text(comp1, SWT.SINGLE | SWT.BORDER);
+		
+		// Text (float) Field - Z Step
 		Text txtZStep = new Text(comp1, SWT.SINGLE | SWT.BORDER);
+		
+		// Make text box stretch to fill width of view
 		txtZStart.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
 		txtZStop.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
 		txtZStep.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
 		
-		// Composite - Steps in S Container
+		// Composite - 'Steps in S' Container
 		Composite comp2 = new Composite(grpCalParams, SWT.NONE);
+		// Each row has 2 cells: label followed by a text box
 		comp2.setLayout(new GridLayout(2, false));
 		comp2.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, false));
 
-		// Steps in S
+		// Text (float) Field - Steps in S
 		(new Label(comp2, SWT.NONE)).setText("Steps in S");
 		Text txtStepsS = new Text(comp2, SWT.SINGLE | SWT.BORDER);
 		txtStepsS.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
@@ -178,22 +220,25 @@ public class IdDescForm extends ViewPart {
 		// Group - Apple Symmetric only parameters
 		Group grpAppleSymOnly = new Group(parent, SWT.NONE);
 		grpAppleSymOnly.setText("APPLE Symmetric only");
+		// Each row has 2 cells: label followed by a text box
 		grpAppleSymOnly.setLayout(new GridLayout(2, false));
 		grpAppleSymOnly.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, false));
 		
 		// Text (float) Field - End gap
 		(new Label(grpAppleSymOnly, SWT.NONE)).setText("End gap");
 		Text txtEndGap = new Text(grpAppleSymOnly, SWT.SINGLE | SWT.BORDER);
-		txtEndGap.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
 		
 		// Text (float) Field - Phasing gap
 		(new Label(grpAppleSymOnly, SWT.NONE)).setText("Phasing gap");
 		Text txtPhasingGap = new Text(grpAppleSymOnly, SWT.SINGLE | SWT.BORDER);
-		txtPhasingGap.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
 
 		// Text (float) Field - Clamp cut
 		(new Label(grpAppleSymOnly, SWT.NONE)).setText("Clamp cut");
 		Text txtClampCut = new Text(grpAppleSymOnly, SWT.SINGLE | SWT.BORDER);
+		
+		// Make text box stretch to fill width of view
+		txtEndGap.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+		txtPhasingGap.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
 		txtClampCut.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
 	}
 	
