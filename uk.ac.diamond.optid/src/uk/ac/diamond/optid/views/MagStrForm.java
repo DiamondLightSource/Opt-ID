0839f26393e74b95a695ef4a2d288afae44123f0
dtailor17 <dharmesh.tailor@live.co.uk>
2015-09-13 23:55:14 +0100

    Created new view part for magnet strengths

---
+++ uk.ac.diamond.optid/src/uk/ac/diamond/optid/views/MagStrForm.java
@@ -0,0 +1,16 @@
+package uk.ac.diamond.optid.views;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.part.ViewPart;
+
+public class MagStrForm extends ViewPart {
+
+	@Override
+	public void createPartControl(Composite parent) {
+	}
+
+	@Override
+	public void setFocus() {
+	}
+
+}
