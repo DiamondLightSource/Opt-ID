5f9dda3043f69726983b875a005d52ea6825880f
Mark Basham <mark.basham@diamond.ac.uk>
2017-03-10 16:16:46 +0000

    Fixed up to use a more generic cluster runner

--- IDSort/src/mpijob.sh
+++ IDSort/src/mpijob.sh
@@ -4,8 +4,6 @@ module load python/ana
 source activate mpi2
 module load openmpi/1.6.5
 
-wdir=/home/gdy32713/DAWN_stable/optid/Opt-ID/IDSort/src
-
 UNIQHOSTS=${TMPDIR}/machines-u
 awk '{print $1 }' ${PE_HOSTFILE} | uniq > ${UNIQHOSTS}
 uniqslots=$(wc -l <${UNIQHOSTS})
@@ -20,6 +18,5 @@ echo "Processes running are : ${processes}"
 mpirun -np ${processes} \
         -x LD_LIBRARY_PATH \
         --hostfile ${UNIQHOSTS} \
-        --wd /dls/tmp/gdy32713/I21sort \
         --tag-output \
-        python $wdir/mpi_runner.py $@
+        python $IDHOME/mpi_runner.py $@
