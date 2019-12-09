d9103d9f95f6cd48390286410d602716b2eb4e7b
Yousef Moazzam <yousef.moazzam@diamond.ac.uk>
2019-12-09 15:14:53 +0000

    Test creation of .mag file from JSON file
    Move the process of creating the .mag file to its own function to enable
    testing of it.

---
+++ IDSort/test/magnets_test.py
@@ -0,0 +1,25 @@
+import unittest
+from tempfile import NamedTemporaryFile
+from collections import namedtuple
+from IDSort.src.magnets import process
+
+
+class MagnetsTest(unittest.TestCase):
+
+    def test_process(self):
+
+        options = {
+            'hmags': 'IDSort/data/I03H.sim',
+            'hemags': 'IDSort/data/I03HEC.sim',
+            'htmags': 'IDSort/data/I03HTE.sim',
+            'vmags': None,
+            'vemags': None
+        }
+
+        options_named = namedtuple("options", options.keys())(*options.values())
+        test_mag_filepath = 'IDSort/data/test_data/test_cpmu.mag'
+
+        with open(test_mag_filepath) as old_mag_file, \
+                NamedTemporaryFile() as new_mag_file:
+            process(options_named, [new_mag_file.name])
+            assert new_mag_file.read() == old_mag_file.read()
