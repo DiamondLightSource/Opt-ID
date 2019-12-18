fbede893f4d59d083a5fa7f864322ac024d94183
Yousef Moazzam <yousef.moazzam@diamond.ac.uk>
2019-12-18 09:42:22 +0000

    Test creation of .mag file for shimming

--- IDSort/test/magnets_test.py
+++ IDSort/test/magnets_test.py
@@ -23,3 +23,21 @@ class MagnetsTest(unittest.TestCase):
                 NamedTemporaryFile() as new_mag_file:
             process(options_named, [new_mag_file.name])
             assert new_mag_file.read() == old_mag_file.read()
+
+    def test_process_shim(self):
+
+        options = {
+            'hmags': 'IDSort/data/I03HH.sim',
+            'hemags': 'IDSort/data/I03HEA.sim',
+            'htmags': 'IDSort/data/I03HTD.sim',
+            'vmags': None,
+            'vemags': None
+        }
+
+        options_named = namedtuple("options", options.keys())(*options.values())
+        test_mag_shim_filepath = 'IDSort/data/test_data/test_cpmu_shim.mag'
+
+        with open(test_mag_shim_filepath) as old_mag_shim_file, \
+                NamedTemporaryFile() as new_mag_shim_file:
+            process(options_named, [new_mag_shim_file.name])
+            assert new_mag_shim_file.read() == old_mag_shim_file.read()
