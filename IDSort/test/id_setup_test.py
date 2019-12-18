4ff3120b6b9083d8ae6e9f03ab4a14dc879d1398
Yousef Moazzam <yousef.moazzam@diamond.ac.uk>
2019-12-18 10:10:40 +0000

    Test creation of JSON file for shimming

--- IDSort/test/id_setup_test.py
+++ IDSort/test/id_setup_test.py
@@ -35,3 +35,32 @@ class IDSetupTest(unittest.TestCase):
             new_json = json.load(new_json_file)
             old_json = json.load(old_json_file)
             assert new_json == old_json
+
+    def test_process_hybrid_symmetric_shim(self):
+
+        options = {
+            'periods': 113,
+            'fullmagdims': (50., 30., 5.76),
+            'hemagdims': (50., 30., 4.00),
+            'htmagdims': (50., 30., 1.13),
+            'poledims': (30., 23., 2.95),
+            'interstice': 0.0625,
+            'gap': 5.6,
+            'type': 'Hybrid_Symmetric',
+            'name': 'test_cpmu_shim',
+            'x': (0, 0.1, 1),
+            'z': (0, 0.1, 1),
+            'steps': 5,
+            'endgapsym': 3.0,
+            'terminalgapsymhyb': 3.0
+        }
+
+        options_named = namedtuple("options", options.keys())(*options.values())
+        test_data_filepath = 'IDSort/data/test_data/test_cpmu_shim.json'
+
+        with open(test_data_filepath) as old_json_shim_file, \
+                NamedTemporaryFile() as new_json_shim_file:
+            process(options_named, [new_json_shim_file.name])
+            new_json_shim = json.load(new_json_shim_file)
+            old_json_shim = json.load(old_json_shim_file)
+            assert new_json_shim == old_json_shim
