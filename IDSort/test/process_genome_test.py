1853dddf4ed68e0e3ee6a2dc6c5636bfe85dfb26
Yousef Moazzam <yousef.moazzam@diamond.ac.uk>
2020-01-08 17:01:58 +0000

    Test creation of genome file from inp file for shimming

--- IDSort/test/process_genome_test.py
+++ IDSort/test/process_genome_test.py
@@ -53,3 +53,34 @@ class ProcessGenomeTest(unittest.TestCase):
         finally:
             os.remove(new_genome_h5_filepath)
             os.remove(new_genome_inp_filepath)
+
+    def test_process_create_genome(self):
+
+        test_json_filepath = 'IDSort/data/test_data/test_cpmu.json'
+        test_mag_filepath = 'IDSort/data/test_data/test_cpmu.mag'
+        test_h5_filepath = 'IDSort/data/test_data/test_cpmu.h5'
+        test_inp_filepath = 'IDSort/data/test_data/1.07788212e-08_000_c0833a96b82c.genome.inp'
+
+        options = {
+            'create_genome': True,
+            'readable': False,
+            'analysis': False,
+            'id_filename': test_json_filepath,
+            'magnets_filename': test_mag_filepath,
+            'id_template': test_h5_filepath
+        }
+
+        options_named = namedtuple("options", options.keys())(*options.values())
+        args = [test_inp_filepath]
+
+        old_genome_filepath = 'IDSort/data/test_data/1.07788212e-08_000_c0833a96b82c.genome.inp.genome'
+        new_genome_filename = os.path.split(test_inp_filepath)[1] + '.genome'
+        new_genome_filepath = os.path.join(os.getcwd(), new_genome_filename)
+
+        try:
+            process(options_named, args)
+            with open(old_genome_filepath) as old_genome_file, \
+                    open(new_genome_filepath) as new_genome_file:
+                assert new_genome_file.read() == old_genome_file.read()
+        finally:
+            os.remove(new_genome_filepath)
