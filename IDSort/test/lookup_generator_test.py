4662d38d90401289e3034bd8d7978108b0d55ac0
Yousef Moazzam <yousef.moazzam@diamond.ac.uk>
2020-01-08 11:44:11 +0000

    Test creation of .h5 file for shimming

--- IDSort/test/lookup_generator_test.py
+++ IDSort/test/lookup_generator_test.py
@@ -27,3 +27,23 @@ class LookupGeneratorTest(unittest.TestCase):
                 new_data = new_h5_file.get(dataset).value
                 old_data = old_h5_file.get(dataset).value
                 assert np.allclose(new_data, old_data)
+
+    def test_process_shim(self):
+
+        options = {
+            'periods': 113,
+            'random': True
+        }
+
+        options_named = namedtuple("options", options.keys())(*options.values())
+        test_json_shim_filepath = 'IDSort/data/test_data/test_cpmu_shim.json'
+        test_h5_shim_filepath = 'IDSort/data/test_data/test_cpmu_shim.h5'
+
+        with h5py.File(test_h5_shim_filepath, 'r') as old_h5_shim_file, \
+                NamedTemporaryFile() as mock_h5_shim_file:
+            process(options_named, [test_json_shim_filepath, mock_h5_shim_file.name])
+            new_h5_shim_file = h5py.File(mock_h5_shim_file.name)
+            for dataset in new_h5_shim_file:
+                new_data = new_h5_shim_file.get(dataset).value
+                old_data = old_h5_shim_file.get(dataset).value
+                assert np.allclose(new_data, old_data)
