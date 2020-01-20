7465fcb3070559e809841cb42ede27ac399ee2de
Yousef Moazzam <yousef.moazzam@diamond.ac.uk>
2020-01-20 14:27:53 +0000

    Test genome creation from initial population

--- IDSort/test/mpi_runner_test.py
+++ IDSort/test/mpi_runner_test.py
@@ -59,3 +59,61 @@ class MpiRunnerTest(unittest.TestCase):
 
         finally:
             shutil.rmtree(new_genome_dir)
+
+    def test_process_initial_population(self):
+
+        test_json_filepath = 'IDSort/data/test_data/sort/test_cpmu.json'
+        test_mag_filepath = 'IDSort/data/test_data/sort/test_cpmu.mag'
+        test_h5_filepath = 'IDSort/data/test_data/sort/test_cpmu.h5'
+        options = {
+            'iterations': 3,
+            'id_filename': test_json_filepath,
+            'magnets_filename': test_mag_filepath,
+            'lookup_filename': test_h5_filepath,
+            'setup': 24,
+            'c': 1,
+            'e': 0.0,
+            'restart': True,
+            'max_age': 10,
+            'scale': 10.0,
+            'singlethreaded': True,
+            'seed': True,
+            'seed_value': 30
+        }
+
+        options_named = namedtuple("options", options.keys())(*options.values())
+        genome_dir = 'IDSort/data/test_data/sort'
+        args = [genome_dir]
+
+        test_genome_filepaths = [
+            'IDSort/data/test_data/sort/mpi_runner_initial_population/1.07788212e-08_001_c0833a96b82c.genome',
+            'IDSort/data/test_data/sort/mpi_runner_initial_population/8.37148991e-09_000_a17c6ae2b0ca.genome',
+            'IDSort/data/test_data/sort/mpi_runner_initial_population/8.92986660e-09_000_dfdeaff6511d.genome',
+            'IDSort/data/test_data/sort/mpi_runner_initial_population/9.97165603e-09_000_c9331df26c93.genome'
+        ]
+
+        try:
+            process(options_named, args)
+            genome_filenames = os.listdir(genome_dir)
+            new_population = []
+
+            for test_genome_filepath in test_genome_filepaths:
+                genome_to_compare_with = None
+                test_genome_filename = os.path.split(test_genome_filepath)[1]
+                test_genome_split = test_genome_filename.split('_')
+                test_genome_fitness_and_age = '_'.join(test_genome_split[0:2])
+                for genome_filename in genome_filenames:
+                    if test_genome_fitness_and_age in genome_filename:
+                        genome_to_compare_with = genome_filename
+                        new_population.append(genome_filename)
+                        break
+
+                assert genome_to_compare_with is not None
+
+                with open(os.path.join(genome_dir, genome_to_compare_with)) as new_genome_file, \
+                        open(test_genome_filepath) as test_genome_file:
+                    assert new_genome_file.read() == test_genome_file.read()
+
+        finally:
+            for genome_filename in new_population:
+                os.remove(os.path.join(genome_dir, genome_filename))
