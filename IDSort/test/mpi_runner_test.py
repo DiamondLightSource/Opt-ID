c19484f16d2355b8ec3df7f1c5e5d2d3576a7f97
Joss Whittle <joss.whittle@gmail.com>
2020-06-24 14:05:14 +0100

    Makes mpi_runner_test.py pass both tests by offloading MagLists comparison to MagLists::__eq__

--- IDSort/test/mpi_runner_test.py
+++ IDSort/test/mpi_runner_test.py
@@ -1,10 +1,11 @@
 import unittest
 import shutil
 import os
-from tempfile import mkdtemp
+import pickle
+import tempfile
 from collections import namedtuple
 
-from IDSort.src.mpi_runner import process
+from IDSort.src.mpi_runner import process, MagLists
 
 
 class MpiRunnerTest(unittest.TestCase):
@@ -12,27 +13,27 @@ class MpiRunnerTest(unittest.TestCase):
     def test_process(self):
 
         test_json_filepath = 'IDSort/data/test_data/sort/test_cpmu.json'
-        test_mag_filepath = 'IDSort/data/test_data/sort/test_cpmu.mag'
-        test_h5_filepath = 'IDSort/data/test_data/sort/test_cpmu.h5'
-        options = {
-            'iterations': 3,
-            'id_filename': test_json_filepath,
-            'magnets_filename': test_mag_filepath,
-            'lookup_filename': test_h5_filepath,
-            'setup': 24,
-            'c': 1,
-            'e': 0.0,
-            'restart': False,
-            'max_age': 10,
-            'scale': 10.0,
-            'singlethreaded': True,
-            'seed': True,
-            'seed_value': 30
-        }
+        test_mag_filepath  = 'IDSort/data/test_data/sort/test_cpmu.mag'
+        test_h5_filepath   = 'IDSort/data/test_data/sort/test_cpmu.h5'
 
-        options_named = namedtuple("options", options.keys())(*options.values())
-        new_genome_dir = mkdtemp()
-        args = [new_genome_dir]
+        options = {
+            'iterations'       : 3,
+            'id_filename'      : test_json_filepath,
+            'magnets_filename' : test_mag_filepath,
+            'lookup_filename'  : test_h5_filepath,
+            'setup'            : 24,
+            'c'                : 1,
+            'e'                : 0.0,
+            'restart'          : False,
+            'max_age'          : 10,
+            'scale'            : 10.0,
+            'singlethreaded'   : True,
+            'seed'             : True,
+            'seed_value'       : 30
+        }
+        options_named  = namedtuple("options", options.keys())(*options.values())
+        new_genome_dir = tempfile.mkdtemp()
+        args           = [new_genome_dir]
 
         test_genome_filepaths = [
             'IDSort/data/test_data/sort/mpi_runner_output/1.12875826e-08_000_7c51ecd01f73.genome',
@@ -45,6 +46,8 @@ class MpiRunnerTest(unittest.TestCase):
             process(options_named, args)
             new_genome_filenames = os.listdir(new_genome_dir)
 
+            assert len(new_genome_filenames) > 0
+
             for new_genome_filename in new_genome_filenames:
                 genome_to_compare_with = None
                 genome_fitness = new_genome_filename.split('_')[0]
@@ -55,8 +58,16 @@ class MpiRunnerTest(unittest.TestCase):
                 assert genome_to_compare_with is not None
 
                 with open(os.path.join(new_genome_dir, new_genome_filename), 'rb') as new_genome_file, \
-                        open(genome_to_compare_with, 'rb') as test_genome_file:
-                    assert new_genome_file.read() == test_genome_file.read()
+                                                open(genome_to_compare_with, 'rb') as old_genome_file:
+
+                    old_maglist = pickle.load(old_genome_file)
+                    new_maglist = pickle.load(new_genome_file)
+
+                assert (type(old_maglist) is MagLists)
+                assert (type(new_maglist) is MagLists)
+
+                # Offloads comparison to MagLists::__eq__ method
+                assert old_maglist == new_maglist
 
         finally:
             shutil.rmtree(new_genome_dir)
@@ -64,27 +75,26 @@ class MpiRunnerTest(unittest.TestCase):
     def test_process_initial_population(self):
 
         test_json_filepath = 'IDSort/data/test_data/sort/test_cpmu.json'
-        test_mag_filepath = 'IDSort/data/test_data/sort/test_cpmu.mag'
-        test_h5_filepath = 'IDSort/data/test_data/sort/test_cpmu.h5'
+        test_mag_filepath  = 'IDSort/data/test_data/sort/test_cpmu.mag'
+        test_h5_filepath   = 'IDSort/data/test_data/sort/test_cpmu.h5'
         options = {
-            'iterations': 3,
-            'id_filename': test_json_filepath,
-            'magnets_filename': test_mag_filepath,
-            'lookup_filename': test_h5_filepath,
-            'setup': 24,
-            'c': 1,
-            'e': 0.0,
-            'restart': True,
-            'max_age': 10,
-            'scale': 10.0,
-            'singlethreaded': True,
-            'seed': True,
-            'seed_value': 30
+            'iterations'       : 3,
+            'id_filename'      : test_json_filepath,
+            'magnets_filename' : test_mag_filepath,
+            'lookup_filename'  : test_h5_filepath,
+            'setup'            : 24,
+            'c'                : 1,
+            'e'                : 0.0,
+            'restart'          : True,
+            'max_age'          : 10,
+            'scale'            : 10.0,
+            'singlethreaded'   : True,
+            'seed'             : True,
+            'seed_value'       : 30
         }
-
         options_named = namedtuple("options", options.keys())(*options.values())
-        genome_dir = 'IDSort/data/test_data/sort/mpi_runner_output'
-        args = [genome_dir]
+        genome_dir    = 'IDSort/data/test_data/sort/mpi_runner_output'
+        args          = [genome_dir]
 
         test_genome_filepaths = [
             'IDSort/data/test_data/sort/mpi_runner_initial_population/1.09170425e-08_000_88d39698a4f7.genome',
@@ -95,7 +105,11 @@ class MpiRunnerTest(unittest.TestCase):
 
         try:
             process(options_named, args)
+
             genome_filenames = os.listdir(genome_dir)
+
+            assert len(genome_filenames) > 0
+
             new_population = []
 
             for test_genome_filepath in test_genome_filepaths:
@@ -103,6 +117,7 @@ class MpiRunnerTest(unittest.TestCase):
                 test_genome_filename = os.path.split(test_genome_filepath)[1]
                 test_genome_split = test_genome_filename.split('_')
                 test_genome_fitness_and_age = '_'.join(test_genome_split[0:2])
+
                 for genome_filename in genome_filenames:
                     if test_genome_fitness_and_age in genome_filename:
                         genome_to_compare_with = genome_filename
@@ -112,9 +127,18 @@ class MpiRunnerTest(unittest.TestCase):
                 assert genome_to_compare_with is not None
 
                 with open(os.path.join(genome_dir, genome_to_compare_with), 'rb') as new_genome_file, \
-                        open(test_genome_filepath, 'rb') as test_genome_file:
-                    assert new_genome_file.read() == test_genome_file.read()
+                                                 open(test_genome_filepath, 'rb') as old_genome_file:
+
+                    old_maglist = pickle.load(old_genome_file)
+                    new_maglist = pickle.load(new_genome_file)
+
+                assert (type(old_maglist) is MagLists)
+                assert (type(new_maglist) is MagLists)
+
+                # Offloads comparison to MagLists::__eq__ method
+                assert old_maglist == new_maglist
 
         finally:
+
             for genome_filename in new_population:
                 os.remove(os.path.join(genome_dir, genome_filename))
