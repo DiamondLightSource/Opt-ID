85dfb87cbbbde4ca1e064701fcf4029ca8c0c51a
Joss Whittle <joss.whittle@gmail.com>
2020-06-24 12:18:20 +0100

    Updates process_genome.py human_output::Hybrid_Symmetric condition to remove manual conditionals.

--- IDSort/src/process_genome.py
+++ IDSort/src/process_genome.py
@@ -22,61 +22,36 @@ def human_output(id_info, filename):
         info = json.load(fp)
     
     readable_outfile = (os.path.split(filename)[1]+'.inp')
+
+    # Lookup table for converting mag names to integers
+    mag_types = { 'HT': 5, 'HE': 4, 'VE': 3, 'HH': 2, 'VV': 1 }
     
-    f3 = open(readable_outfile,'w')
-    
-    if info['type']=='Hybrid_Symmetric':
-        
-        #TODO make a proper function somewhere
-        #generate idlist here
-        a=0
-        vv=0
-        hh=0
-        ve=0
-        he=0
-        ht=0
-        for b in range(len(info['beams'])):
-            a=0
-            for mag in info['beams'][b]['mags']:
-                if info['beams'][b]['mags'][a]['type']=='HT':mag_type=5
-                elif info['beams'][b]['mags'][a]['type']=='HE':mag_type=4
-                elif info['beams'][b]['mags'][a]['type']=='VE':mag_type=3
-                elif info['beams'][b]['mags'][a]['type']=='HH':mag_type=2
-                elif info['beams'][b]['mags'][a]['type']=='VV':mag_type=1
-                
-                if mag_type==1:
-                    mag_num=int(maglists.magnet_lists['VV'][vv][0])
-                    mag_flip=maglists.magnet_lists['VV'][vv][1]
-                    vv+=1
-                
-                if mag_type==2:
-                    mag_num=int(maglists.magnet_lists['HH'][hh][0])
-                    mag_flip=maglists.magnet_lists['HH'][hh][1]
-                    hh+=1
-                    
-                if mag_type==3:
-                    mag_num=int(maglists.magnet_lists['VE'][ve][0])
-                    mag_flip=maglists.magnet_lists['VE'][ve][1]
-                    ve+=1
-                    
-                if mag_type==4:
-                    mag_num=int(maglists.magnet_lists['HE'][he][0])
-                    mag_flip=maglists.magnet_lists['HE'][he][1]
-                    he+=1
-                    
-                if mag_type==5:
-                    mag_num=int(maglists.magnet_lists['HT'][ht][0])
-                    mag_flip=maglists.magnet_lists['HT'][ht][1]
-                    ht+=1
-                
-                line= ("%5i %4i %4i %4i %4i %03i\n"%(b+1,a+1,mag_type,info['beams'][b]['mags'][a]['direction_matrix'][2][2],mag_flip, mag_num))
-                f3.write(line)
-                
-                a+=1
-                
-            f3.write("\n")
-        
-        f3.close()
+    if info['type'] == 'Hybrid_Symmetric':
+        # TODO make a proper function somewhere
+
+        with open(readable_outfile, 'w') as fp:
+
+            # Track the current index of each magnet type
+            mag_indices = { 'HT': 0, 'HE': 0, 'VE': 0, 'HH': 0, 'VV': 0 }
+
+            for b, beam in enumerate(info['beams']):
+                for a, mag in enumerate(beam['mags']):
+
+                    # Prepare data for this magnet
+                    mag_type = mag_types[mag['type']]
+                    mag_data = maglists.magnet_lists[mag['type']][mag_indices[mag['type']]]
+                    mag_num  = int(mag_data[0])
+                    mag_flip = mag_data[1]
+
+                    # Update the index to the next magnet of this type
+                    mag_indices[mag['type']] += 1
+
+                    # Write each magnet in the beam as a new line
+                    fp.write('%5i %4i %4i %4i %4i %03i\n' % ((b + 1), (a + 1),
+                                                             mag_type, mag['direction_matrix'][2][2],
+                                                             mag_flip, mag_num))
+                # New line at the end of the beam
+                fp.write('\n')
     
     if info['type']=='PPM_AntiSymmetric':
         
