3481d664ad6b2c8715cf43cddba24685c5bd7b4c
Joss Whittle <joss.whittle@gmail.com>
2020-06-25 14:04:58 +0100

    Refactors compare.py and compare_test.py to use a new data directory structure and safely handle failed tests.
    On failed test case observed output files are left in the directory for inspection.

--- IDSort/test/compare_test.py
+++ IDSort/test/compare_test.py
@@ -1,4 +1,4 @@
-import os
+import os, shutil
 import unittest
 from collections import namedtuple
 
@@ -8,23 +8,56 @@ from IDSort.src.compare import process
 class CompareTest(unittest.TestCase):
 
     def test_process(self):
+        # inp == Inputs
+        # exp == Expected Outputs
+        # obs == Observed Outputs
 
-        test_original_genome_filepath = 'IDSort/data/test_data/sort/mpi_runner_output/1.12875826e-08_000_7c51ecd01f73.genome'
-        test_shimmed_genome_filepath = 'IDSort/data/test_data/shim/1.20847271e-07_000_A85a13cc425d0.genome'
-        test_shim_filepath =  'IDSort/data/test_data/shim/test_shim.txt'
-        new_shim_filename = 'shim'
+        data_path = 'IDSort/test/data/compare_test'
+        inp_path  = os.path.join(data_path, 'inputs')
+        exp_path  = os.path.join(data_path, 'expected_outputs')
+        obs_path  = os.path.join(data_path, 'observed_outputs')
+
+        # Prepare input file paths
+        inp_original_genome_path = os.path.join(inp_path, 'sort/1.12875826e-08_000_7c51ecd01f73.genome')
+        inp_shimmed_genome_path  = os.path.join(inp_path, 'shim/1.20847271e-07_000_A85a13cc425d0.genome')
+
+        # Prepare expected output file paths
+        exp_shim_comparison_path = os.path.join(exp_path, 'output.txt')
+
+        # Prepare observed output file paths
+        obs_shim_comparison_path = os.path.join(obs_path, 'output.txt')
+
+        # Always clear any observed output files before running test
+        shutil.rmtree(obs_path, ignore_errors=True)
+        os.makedirs(obs_path)
+
+        # TODO refactor parameters for process function to use named tuple
+        # Prepare parameters for process function
         options = {}
         options_named = namedtuple("options", options.keys())(*options.values())
         args = [
-            test_original_genome_filepath,
-            test_shimmed_genome_filepath,
-            new_shim_filename
+            inp_original_genome_path,
+            inp_shimmed_genome_path,
+            obs_shim_comparison_path
         ]
 
         try:
+
+            # Execute the function under test
             process(options_named, args)
-            with open(test_shim_filepath) as old_shim_file, \
-                    open(new_shim_filename + '.txt') as new_shim_file:
-                assert new_shim_file.read() == old_shim_file.read()
-        finally:
-            os.remove(new_shim_filename + '.txt')
+
+            # Compare the output file to the expected one
+            with open(exp_shim_comparison_path) as exp_shim_file, \
+                 open(obs_shim_comparison_path) as obs_shim_file:
+
+                # TODO make comparison more robust based on values not bytes (pandas .csv instead of manual .txt ?)
+                # Bytewise file comparison works for now for raw text files
+                assert exp_shim_file.read().strip() == obs_shim_file.read().strip()
+
+        # Use (except + else) instead of (finally) so that output files can be inspected if the test fails
+        except Exception as ex: raise ex
+        else:
+
+            # Clear any observed output files after running successful test
+            shutil.rmtree(obs_path, ignore_errors=True)
+            os.makedirs(obs_path)
