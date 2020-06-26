c25e36bc3ba503b11301c6d43b92eaa1226a48dd
Joss Whittle <joss.whittle@gmail.com>
2020-06-26 12:14:53 +0100

    Updates lookup_generator.py to use closures on file opening.

--- IDSort/src/lookup_generator.py
+++ IDSort/src/lookup_generator.py
@@ -23,6 +23,8 @@ import numpy as np
 import h5py
 import json
 
+# TODO refactor this file
+
 def fortPMB_NEW(testpoint,m,i,magdims, V1):
     '''
     This function Calculates the B-field in a single orientation according to the calling function
@@ -119,9 +121,9 @@ def wrapCalcB(testpoint, magdims,  V1):
 def process(options, args):
     # Add all the magnets
 
-    fp = open(args[0], 'r')
-    data = json.load(fp)
-    fp.close()
+    # TODO refactor arguments to accept json file as named parameter
+    with open(args[0], 'r') as fp:
+        data = json.load(fp)
 
     # create calculation array
     #meshgrid modified 18/02/19 ZP+MB to calculate no of points in each direction properly (avoid floating point errors)
@@ -137,58 +139,56 @@ def process(options, args):
     print("smin %f"%(data['smin']))
     print("smax %f"%(data['smax']))
     print("sstep %f"%(data['sstep']))
-    outfile = h5py.File(args[1], 'w')
-    
+
     if data['type'] == 'PPM_AntiSymmetric' or data['type'] == 'Hybrid_Symmetric':
-    
-        for b in range(len(data['beams'])):
-            count = 0
-            print("Processing beam %02i" % (b))
-            datashape = (testpoints.shape[1], testpoints.shape[2], testpoints.shape[3], 3, 3, len(data['beams'][b]['mags']))
-            #print("testpoints.shape[3] %s"%(testpoints.shape[3]))
-            chunkshape = (testpoints.shape[1], testpoints.shape[2], testpoints.shape[3], 3, 3, 1)
-            #print ("datashape is : " + s(datashape))
-            ds = outfile.create_dataset(data['beams'][b]['name'], shape=datashape, dtype=np.float64, chunks=chunkshape)
-    
-            for mag in data['beams'][b]['mags']:
-                print("processing beam %02i magnet %04i" % (b, count))
-                dataset = wrapCalcB(testpoints, np.array(mag['dimensions']), np.array(mag['position']))
-                ds[:, :, :, :, :, count] = dataset.dot(np.array(mag['direction_matrix']))
-                count += 1
-                
-        outfile.close()
+
+        with h5py.File(args[1], 'w') as outfile:
+
+            for b in range(len(data['beams'])):
+                count = 0
+                print("Processing beam %02i" % (b))
+                datashape = (testpoints.shape[1], testpoints.shape[2], testpoints.shape[3], 3, 3, len(data['beams'][b]['mags']))
+                #print("testpoints.shape[3] %s"%(testpoints.shape[3]))
+                chunkshape = (testpoints.shape[1], testpoints.shape[2], testpoints.shape[3], 3, 3, 1)
+                #print ("datashape is : " + s(datashape))
+                ds = outfile.create_dataset(data['beams'][b]['name'], shape=datashape, dtype=np.float64, chunks=chunkshape)
+
+                for mag in data['beams'][b]['mags']:
+                    print("processing beam %02i magnet %04i" % (b, count))
+                    dataset = wrapCalcB(testpoints, np.array(mag['dimensions']), np.array(mag['position']))
+                    ds[:, :, :, :, :, count] = dataset.dot(np.array(mag['direction_matrix']))
+                    count += 1
         
     if data['type'] == 'APPLE_Symmetric':
-    
-        for b in range(len(data['beams'])):
-            count = 0
-            print("Processing beam %02i" % (b))
-            datashape = (testpoints.shape[1], testpoints.shape[2], testpoints.shape[3], 3, 3, len(data['beams'][b]['mags']))
-            chunkshape = (testpoints.shape[1], testpoints.shape[2], testpoints.shape[3], 3, 3, 1)
-            #print ("datashape is : " + str(datashape))
-            ds = outfile.create_dataset(data['beams'][b]['name'], shape=datashape, dtype=np.float64, chunks=chunkshape)
-    
-            for mag in data['beams'][b]['mags']:
-                print("processing beam %02i magnet %04i" % (b, count))
-                
-                datasetblock = wrapCalcB(testpoints, np.array(mag['dimensions']), np.array(mag['position']))
-                if b%2==1:
-                    c2pos = np.array(mag['position'])+np.array([mag['dimensions'][0]-data['clampcut'],mag['dimensions'][1]-data['clampcut'],0.0])
-                    datasetc1 = wrapCalcB(testpoints, np.array([data['clampcut'],data['clampcut'],mag['dimensions'][2]]), np.array(mag['position']))
-                    datasetc2 = wrapCalcB(testpoints, np.array([data['clampcut'],data['clampcut'],mag['dimensions'][2]]), c2pos)
-                if b%2==0:
-                    c1pos = np.array(mag['position'])+np.array([mag['dimensions'][0]-data['clampcut'], 0.0, 0.0])
-                    c2pos = np.array(mag['position'])+np.array([0.0 ,mag['dimensions'][1]-data['clampcut'], 0.0])
-                    datasetc1 = wrapCalcB(testpoints, np.array([data['clampcut'],data['clampcut'],mag['dimensions'][2]]), c1pos)
-                    datasetc2 = wrapCalcB(testpoints, np.array([data['clampcut'],data['clampcut'],mag['dimensions'][2]]), c2pos)
-                dataset=datasetblock-datasetc1-datasetc2
-                
-                ds[:, :, :, :, :, count] = dataset.dot(np.array(mag['direction_matrix']))
-#                ds[:, :, :, :, :, count] = np.array(mag['direction_matrix']).dot(dataset)
-                count += 1
-                
-        outfile.close()
 
+        with h5py.File(args[1], 'w') as outfile:
+
+            for b in range(len(data['beams'])):
+                count = 0
+                print("Processing beam %02i" % (b))
+                datashape = (testpoints.shape[1], testpoints.shape[2], testpoints.shape[3], 3, 3, len(data['beams'][b]['mags']))
+                chunkshape = (testpoints.shape[1], testpoints.shape[2], testpoints.shape[3], 3, 3, 1)
+                #print ("datashape is : " + str(datashape))
+                ds = outfile.create_dataset(data['beams'][b]['name'], shape=datashape, dtype=np.float64, chunks=chunkshape)
+
+                for mag in data['beams'][b]['mags']:
+                    print("processing beam %02i magnet %04i" % (b, count))
+
+                    datasetblock = wrapCalcB(testpoints, np.array(mag['dimensions']), np.array(mag['position']))
+                    if b%2==1:
+                        c2pos = np.array(mag['position'])+np.array([mag['dimensions'][0]-data['clampcut'],mag['dimensions'][1]-data['clampcut'],0.0])
+                        datasetc1 = wrapCalcB(testpoints, np.array([data['clampcut'],data['clampcut'],mag['dimensions'][2]]), np.array(mag['position']))
+                        datasetc2 = wrapCalcB(testpoints, np.array([data['clampcut'],data['clampcut'],mag['dimensions'][2]]), c2pos)
+                    if b%2==0:
+                        c1pos = np.array(mag['position'])+np.array([mag['dimensions'][0]-data['clampcut'], 0.0, 0.0])
+                        c2pos = np.array(mag['position'])+np.array([0.0 ,mag['dimensions'][1]-data['clampcut'], 0.0])
+                        datasetc1 = wrapCalcB(testpoints, np.array([data['clampcut'],data['clampcut'],mag['dimensions'][2]]), c1pos)
+                        datasetc2 = wrapCalcB(testpoints, np.array([data['clampcut'],data['clampcut'],mag['dimensions'][2]]), c2pos)
+                    dataset=datasetblock-datasetc1-datasetc2
+
+                    ds[:, :, :, :, :, count] = dataset.dot(np.array(mag['direction_matrix']))
+    #                ds[:, :, :, :, :, count] = np.array(mag['direction_matrix']).dot(dataset)
+                    count += 1
 
 if __name__ == "__main__":
     import optparse
@@ -197,7 +197,7 @@ if __name__ == "__main__":
 #    parser.add_option("-o", "--output", dest="output", help="Select the file to write the output to", default=None)
     parser.add_option("-p", "--periods", dest="periods", help="Set the number of full periods for the Device", default=5, type="int")
     parser.add_option("-s", "--symmetric", dest="symmetric", help="Set the device to symmetric rather then Anti-symmetric", action="store_true", default=False)
-    parser.add_option("-r", "--random", dest="random", help="Choose the magnets randomly instead of sequentialy", action="store_true", default=False)
+    parser.add_option("-r", "--random", dest="random", help="Choose the magnets randomly instead of sequentially", action="store_true", default=False)
     parser.add_option("-v", "--verbose", dest="verbose", help="display debug information", action="store_true", default=False)
 
     (options, args) = parser.parse_args()
