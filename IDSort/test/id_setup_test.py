8837d26aa63cf7e1b79b4e44b968875def797c95
Joss Whittle <joss.whittle@gmail.com>
2020-07-17 10:26:34 +0100

    Added test cases for id_setup_test.py to ensure refactor is consistent for APPLE symmetric and PPM antisymmetric devices as original test only covered Hybrid symmetric.

--- IDSort/test/id_setup_test.py
+++ IDSort/test/id_setup_test.py
@@ -8,6 +8,124 @@ from ..src.id_setup import process
 
 class IDSetupTest(unittest.TestCase):
 
+    def test_process_apple_symmetric(self):
+        # exp == Expected Outputs
+        # obs == Observed Outputs
+
+        data_path = 'IDSort/test/data/id_setup_test/test_process_apple_symmetric'
+        exp_path  = os.path.join(data_path, 'expected_outputs')
+        obs_path  = os.path.join(data_path, 'observed_outputs')
+
+        # Prepare expected output file paths
+        exp_json_path = os.path.join(exp_path, 'test_apple.json')
+
+        # Prepare observed output file paths
+        obs_json_path = os.path.join(obs_path, 'test_apple.json')
+
+        # Always clear any observed output files before running test
+        shutil.rmtree(obs_path, ignore_errors=True)
+        os.makedirs(obs_path)
+
+        # Prepare parameters for process function
+        options = {
+            'periods'           : 113,
+            'fullmagdims'       : (41., 16., 6.22),
+            'hemagdims'         : (41., 16., 4.0),
+            'vemagdims'         : (41., 16., 3.12),
+            'interstice'        : 0.04,
+            'clampcut'          : 5.0,
+            'gap'               : 5.1,
+            'type'              : 'APPLE_Symmetric',
+            'name'              : 'test_apple',
+            'x'                 : (-2.0, 2.1, 2.5),
+            'z'                 : (-0.0, 0.1, 0.1),
+            'steps'             : 1,
+            'endgapsym'         : 5.0,
+            'phasinggap'        : 0.5,
+        }
+        options_named = namedtuple("options", options.keys())(*options.values())
+
+        try:
+
+            # Execute the function under test
+            process(options_named, [obs_json_path])
+
+            # Compare the output file to the expected one
+            with open(exp_json_path, 'r') as exp_json_file, \
+                 open(obs_json_path, 'r') as obs_json_file:
+
+                exp_json = json.load(exp_json_file)
+                obs_json = json.load(obs_json_file)
+
+                # dict::__eq__ works for json loaded dictionaries
+                assert exp_json == obs_json
+
+        # Use (except + else) instead of (finally) so that output files can be inspected if the test fails
+        except Exception as ex: raise ex
+        else:
+
+            # Clear any observed output files after running successful test
+            shutil.rmtree(obs_path, ignore_errors=True)
+            os.makedirs(obs_path)
+
+    def test_process_ppm_antisymmetric(self):
+        # exp == Expected Outputs
+        # obs == Observed Outputs
+
+        data_path = 'IDSort/test/data/id_setup_test/test_process_ppm_antisymmetric'
+        exp_path  = os.path.join(data_path, 'expected_outputs')
+        obs_path  = os.path.join(data_path, 'observed_outputs')
+
+        # Prepare expected output file paths
+        exp_json_path = os.path.join(exp_path, 'test_antippm.json')
+
+        # Prepare observed output file paths
+        obs_json_path = os.path.join(obs_path, 'test_antippm.json')
+
+        # Always clear any observed output files before running test
+        shutil.rmtree(obs_path, ignore_errors=True)
+        os.makedirs(obs_path)
+
+        # Prepare parameters for process function
+        # TODO these parameters are made up to be consistent but are not from a real device
+        options = {
+            'periods'           : 113,
+            'fullmagdims'       : (41., 16., 6.22),
+            'hemagdims'         : (41., 16., 4.0),
+            'vemagdims'         : (41., 16., 3.12),
+            'interstice'        : 0.04,
+            'gap'               : 5.1,
+            'type'              : 'PPM_AntiSymmetric',
+            'name'              : 'test_antippm',
+            'x'                 : (-2.0, 2.1, 2.5),
+            'z'                 : (-0.0, 0.1, 0.1),
+            'steps'             : 1,
+        }
+        options_named = namedtuple("options", options.keys())(*options.values())
+
+        try:
+
+            # Execute the function under test
+            process(options_named, [obs_json_path])
+
+            # Compare the output file to the expected one
+            with open(exp_json_path, 'r') as exp_json_file, \
+                 open(obs_json_path, 'r') as obs_json_file:
+
+                exp_json = json.load(exp_json_file)
+                obs_json = json.load(obs_json_file)
+
+                # dict::__eq__ works for json loaded dictionaries
+                assert exp_json == obs_json
+
+        # Use (except + else) instead of (finally) so that output files can be inspected if the test fails
+        except Exception as ex: raise ex
+        else:
+
+            # Clear any observed output files after running successful test
+            shutil.rmtree(obs_path, ignore_errors=True)
+            os.makedirs(obs_path)
+
     def test_process_hybrid_symmetric(self):
         # exp == Expected Outputs
         # obs == Observed Outputs
