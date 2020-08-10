515acf000b27f53a1e2d01226dc35f03badbc17c
Joss Whittle <joss.whittle@gmail.com>
2020-08-10 13:25:16 +0100

    Removes deprecated Opt-ID.py and Opt-ID.sh that should have been removed during the refactor. optid.py was incorrectly removed instead, and has now been re-added although will need further refactoring to work correctly.

--- IDSort/src/Opt-ID.sh
+++
@@ -1,8 +0,0 @@
-echo "Hostname is "
-echo $HOSTNAME
-
-. /etc/profile.d/modules.sh
-module load python/ana
-which python
-echo python /home/ssg37927/ID/Opt-ID/IDSort/src/v2/Opt-ID.py $@ 
-time python /home/ssg37927/ID/Opt-ID/IDSort/src/v2/Opt-ID.py $@ 
